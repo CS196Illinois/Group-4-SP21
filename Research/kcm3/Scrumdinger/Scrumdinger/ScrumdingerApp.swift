91da93cfa77684796f9677540a13b7bff3481a4b
Cooper <kcoopermiller9@gmail.com>
2021-02-28 14:19:42 -0600

    completed Using Stacks to Arrange Views tutorial

---
+++ Research/kcm3/Scrumdinger/Scrumdinger/ScrumdingerApp.swift
@@ -0,0 +1,17 @@
+//
+//  ScrumdingerApp.swift
+//  Scrumdinger
+//
+//  Created by Cooper Miller on 2/28/21.
+//
+
+import SwiftUI
+
+@main
+struct ScrumdingerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            MeetingView()
+        }
+    }
+}
