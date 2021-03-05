a2f7282fb3ecd48f6c17f55f6eea149eb3cff3f2
Cooper <kcoopermiller9@gmail.com>
2021-03-04 22:02:11 -0600

    completed Displaying Data in a List tutorial

---
+++ Research/kcm3/Scrumdinger/Scrumdinger/ScrumsView.swift
@@ -0,0 +1,27 @@
+//
+//  ScrumsView.swift
+//  Scrumdinger
+//
+//  Created by Cooper Miller on 3/4/21.
+//
+
+import SwiftUI
+
+struct ScrumsView: View {
+    let scrums: [DailyScrum]
+    
+    var body: some View {
+        List {
+            ForEach(scrums) { scrum in
+                CardView(scrum: scrum)
+                    .listRowBackground(scrum.color)
+            }
+        }
+    }
+}
+
+struct ScrumsView_Previews: PreviewProvider {
+    static var previews: some View {
+        ScrumsView(scrums: DailyScrum.data)
+    }
+}
