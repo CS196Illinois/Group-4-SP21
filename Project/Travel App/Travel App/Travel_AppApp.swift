d9359c84744e8eaa082c9f974090e490805e2afe
Matt Geimer <mgeimer2@illinois.edu>
2021-03-14 10:24:15 -0500

    feat: Initial project files

---
+++ Project/Travel App/Travel App/Travel_AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  Travel_AppApp.swift
+//  Travel App
+//
+//  Created by Matthew Geimer on 3/14/21.
+//
+
+import SwiftUI
+
+@main
+struct Travel_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
