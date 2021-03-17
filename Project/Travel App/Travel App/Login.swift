6400adfa4a41b35f8eacf17c7728da18ac55e16a
Cooper <kcoopermiller9@gmail.com>
2021-03-16 23:36:05 -0500

    feat: completed simple login page

--- Project/Travel App/Travel App/Login.swift
+++ Project/Travel App/Travel App/Login.swift
@@ -42,7 +42,39 @@ struct Login : View {
                 .background(RoundedRectangle(cornerRadius: 4).stroke(self.password != "" ? Color("Color") : self.color, lineWidth: 2))
             .padding(.top, 25)
             
+            HStack {
+                Spacer()
+                
+                Button(action: {
+                    
+                }) {
+                    Text("Forget password")
+                        .fontWeight(.bold)
+                        .foregroundColor(Color("Color"))
+                }
+            }
+            .padding(.top, 20)
+            
+            Button(action: {
+                
+            }) {
+                Text("Sign in")
+                    .foregroundColor(.white)
+                    .padding(.vertical)
+                    .frame(width: UIScreen.main.bounds.width - 50)
+            }
+            .background(Color("Color"))
+            .cornerRadius(10)
+            .padding(.top, 25)
+            
+            Button(action: {
+            }) {
+                Text("Register")
+                    .fontWeight(/*@START_MENU_TOKEN@*/.bold/*@END_MENU_TOKEN@*/)
+                    .foregroundColor(Color("Color"))
+            }
+            .padding()
         }
-        .padding(.horizontal, 25)
+            .padding(.horizontal, 25)
     }
 }
