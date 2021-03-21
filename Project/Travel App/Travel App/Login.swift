8679f89b8c3a9665539dd40ec832bac92f3766bd
Cooper <kcoopermiller9@gmail.com>
2021-03-20 21:52:43 -0500

    feat: added ErrorView

--- Project/Travel App/Travel App/Login.swift
+++ Project/Travel App/Travel App/Login.swift
@@ -7,76 +7,95 @@ struct Login : View {
     @State var password = ""
     @State var visible = false
     @Binding var show : Bool
+    @State var alert = false
+    @State var error = ""
     
     var body : some View {
-        VStack {
-            Text("🧇")
-                .font(.system(size: 100))
+        ZStack {
+            VStack {
+                Text("🧇")
+                    .font(.system(size: 100))
+                    .padding()
+                
+                Text("Log in to your account")
+                    .font(.title)
+                    .fontWeight(/*@START_MENU_TOKEN@*/.bold/*@END_MENU_TOKEN@*/)
+                    .foregroundColor(self.color)
+                    .padding(.top, 35)
+                
+                TextField("Email", text: self.$email)
                 .padding()
-            
-            Text("Log in to your account")
-                .font(.title)
-                .fontWeight(/*@START_MENU_TOKEN@*/.bold/*@END_MENU_TOKEN@*/)
-                .foregroundColor(self.color)
-                .padding(.top, 35)
-            
-            TextField("Email", text: self.$email)
-            .padding()
-                .background(RoundedRectangle(cornerRadius: 4).stroke(self.email != "" ? Color("Color") : self.color, lineWidth: 2))
-            .padding(.top, 25)
-            
-            HStack(spacing: 15) {
-                VStack {
-                    if self.visible {
-                        TextField("Password", text: self.$password)
+                    .background(RoundedRectangle(cornerRadius: 4).stroke(self.email != "" ? Color("Color") : self.color, lineWidth: 2))
+                .padding(.top, 25)
+                
+                HStack(spacing: 15) {
+                    VStack {
+                        if self.visible {
+                            TextField("Password", text: self.$password)
+                        }
+                        else {
+                            SecureField("Password", text: self.$password)
+                        }
                     }
-                    else {
-                        SecureField("Password", text: self.$password)
+                    Button(action: {self.visible.toggle()}) {
+                        Image(systemName: self.visible ? "eye.slash.fill" : "eye.fill" )
+                            .foregroundColor(self.color)
                     }
                 }
-                Button(action: {self.visible.toggle()}) {
-                    Image(systemName: self.visible ? "eye.slash.fill" : "eye.fill" )
-                        .foregroundColor(self.color)
+                .padding()
+                    .background(RoundedRectangle(cornerRadius: 4).stroke(self.password != "" ? Color("Color") : self.color, lineWidth: 2))
+                .padding(.top, 25)
+                
+                HStack {
+                    Spacer()
+                    
+                    Button(action: {
+                        
+                    }) {
+                        Text("Forget password")
+                            .fontWeight(.bold)
+                            .foregroundColor(Color("Color"))
+                    }
                 }
-            }
-            .padding()
-                .background(RoundedRectangle(cornerRadius: 4).stroke(self.password != "" ? Color("Color") : self.color, lineWidth: 2))
-            .padding(.top, 25)
-            
-            HStack {
-                Spacer()
+                .padding(.top, 20)
                 
                 Button(action: {
                     
+                    self.verify()
+                    
                 }) {
-                    Text("Forget password")
-                        .fontWeight(.bold)
+                    Text("Sign in")
+                        .foregroundColor(.white)
+                        .padding(.vertical)
+                        .frame(width: UIScreen.main.bounds.width - 50)
+                }
+                .background(Color("Color"))
+                .cornerRadius(10)
+                .padding(.top, 25)
+                
+                Button(action: {
+                    self.show.toggle()
+                }) {
+                    Text("Register")
+                        .fontWeight(/*@START_MENU_TOKEN@*/.bold/*@END_MENU_TOKEN@*/)
                         .foregroundColor(Color("Color"))
                 }
+                .padding()
             }
-            .padding(.top, 20)
-            
-            Button(action: {
-                
-            }) {
-                Text("Sign in")
-                    .foregroundColor(.white)
-                    .padding(.vertical)
-                    .frame(width: UIScreen.main.bounds.width - 50)
-            }
-            .background(Color("Color"))
-            .cornerRadius(10)
-            .padding(.top, 25)
-            
-            Button(action: {
-                self.show.toggle()
-            }) {
-                Text("Register")
-                    .fontWeight(/*@START_MENU_TOKEN@*/.bold/*@END_MENU_TOKEN@*/)
-                    .foregroundColor(Color("Color"))
-            }
-            .padding()
-        }
             .padding(.horizontal, 25)
+            
+            if self.alert{
+                ErrorView(alert: self.$alert, error: self.$error)
+            }
+        }
+    }
+    func verify() {
+        if self.email != "" && self.password != "" {
+            
+        }
+        else {
+            self.error = "Please enter email and password"
+            self.alert.toggle()
+        }
     }
 }
