2b11373608a89bb0ca04fb64a8276ad50d524df1
harshagarwal <harshagarwal2002@gmail.com>
2021-05-01 00:40:42 +0530

    Issues fixed

--- Project/Travel App/Travel App/ContentView.swift
+++ Project/Travel App/Travel App/ContentView.swift
@@ -5,7 +5,33 @@ struct ContentView: View {
     @State var status = UserDefaults.standard.value(forKey: "status") as? Bool ?? false
     
     var body: some View {
-        ApiView()
+        NavigationView{
+                   VStack {
+                       if self.status {
+                           //homescreen
+                       }
+                       else {
+                           ZStack{
+                               NavigationLink(destination: SignUp(show: self.$show), isActive: self.$show) {
+                                   Text("")
+                               }
+                               .hidden()
+                               Login(show: self.$show)
+                           }
+                           .navigationBarTitle("")
+                           .navigationBarHidden(true)
+                           .navigationBarBackButtonHidden(true)
+                           .onAppear() {
+                               NotificationCenter.default.addObserver(forName: NSNotification.Name("status"), object: nil, queue: .main) { (_) in
+
+                                    self.status = UserDefaults.standard.value(forKey: "status") as? Bool ?? false
+                               }
+                           }
+                       }
+                   }
+               }
+
+//        ApiView()
     }
 }
 
